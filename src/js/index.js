3f7abc28d9be6c565a2b5149de09b8627106df9f
Saeki, Takahiro <ts-takahiro.saeki@rakuten.com>
2017-12-14 11:25:16 +0900

    fixed: slightly improvement.

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,17 @@
 import 'babel-polyfill';
 import { h, render } from 'preact';
-import { AppContainer } from 'react-hot-loader';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'preact-redux'
-import {
-  devToolsEnhancer,
-  composeWithDevTools
-} from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import 'todomvc-app-css/index.css';
 import App from './containers/App'
 import reducer from './reducers';
-import 'todomvc-app-css/index.css';
 import storage from './domain/storage';
 
 const store = createStore(
   reducer, 
   storage.read() ? { todo: storage.read() } : {}, 
-  composeWithDevTools()
+  process.env.NODE_ENV === 'production' ? undefined : composeWithDevTools()
 )
 
 render(
